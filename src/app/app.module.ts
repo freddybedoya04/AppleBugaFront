88cf47d2246034e84eb9ec35183cdb8ab7e5de14
Freddy Bedoya <freddybedoya04@gmail.com>
2023-04-16 12:57:13 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
+import { VentasComponent } from './components/Dashboard/ventas/ventas.component';
+import { ReportesComponent } from './components/Dashboard/reportes/reportes.component';
+import { UsuariosComponent } from './components/Dashboard/usuarios/usuarios.component';
+import { SharedModule } from './components/shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ErrorComponent,
+    VentasComponent,
+    ReportesComponent,
+    UsuariosComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
